54c38d3d3e5c1a603213dee7e33b229665c71190
ERIK BARBOSA <135337607+Erikbarbosa-R@users.noreply.github.com>
2025-09-14 21:12:11 -0300

    feat: atualizar App.tsx principal
    - Integra novos contextos e providers
    - Melhora estrutura da aplicação principal
    - Adiciona suporte para novos fluxos

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Provider } from 'react-redux';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { store } from './src/store';
 import { AppProvider } from './src/context/AppContext';
 import { FavoritesProvider } from './src/context/FavoritesContext';
 import { KeyboardProvider } from './src/navigation/AppNavigator';
@@ -12,16 +10,14 @@ import { COLORS } from './src/constants';
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Provider store={store}>
-        <AppProvider>
-          <FavoritesProvider>
-            <KeyboardProvider>
-              <StatusBar style="dark" backgroundColor={COLORS.surface} />
-              <AppNavigator />
-            </KeyboardProvider>
-          </FavoritesProvider>
-        </AppProvider>
-      </Provider>
+      <AppProvider>
+        <FavoritesProvider>
+          <KeyboardProvider>
+            <StatusBar style="dark" backgroundColor={COLORS.surface} />
+            <AppNavigator />
+          </KeyboardProvider>
+        </FavoritesProvider>
+      </AppProvider>
     </GestureHandlerRootView>
   );
 }
